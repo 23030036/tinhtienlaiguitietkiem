d193955527176c300d560aa93f2b93ad18ff300b
23030036 <23030036@student.bdu.edu.vn>
2026-06-18 15:42:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.image("logo.jpg")
 # Tiêu đề app
-st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 2_. THỦY TIÊN")
+st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 2_.Ngoc Ha")
 
 # Nhập dữ liệu
 C = st.number_input(
